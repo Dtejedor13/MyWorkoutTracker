0208e95ea3b8c675435f593485cf35fd186a5f62
Dtejedor13 <am4kodum@gmail.com>
2022-01-07 09:56:12 +0100

    add base class libary

---
+++ MyWorkoutTracker/MyWorkoutTracker_Base/Models/MuscleCategory.cs
@@ -0,0 +1,14 @@
+﻿namespace MyWorkoutTracker_Base.Models
+{
+    public class MuscleCategory
+    {
+        public int ID { get; private set; }
+        public string Name { get; private set; }
+
+        public MuscleCategory(int id, string name)
+        {
+            Name = name;
+            ID = id;
+        }
+    }
+}
---
+++ MyWorkoutTracker/MyWorkoutTracker_Base/Models/MuscleGroup.cs
@@ -0,0 +1,16 @@
+﻿namespace MyWorkoutTracker_Base.Models
+{
+    public class MuscleGroup
+    {
+        public int ID { get; private set; }
+        public string Name { get; private set; }
+        public int CategoryID { get; private set; }
+
+        public MuscleGroup(int id, string name, int catID)
+        {
+            ID = id;
+            Name = name;
+            CategoryID = catID;
+        }
+    }
+}
